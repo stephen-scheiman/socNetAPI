74a6442f1c2cb3e9431e69c75563090188177a7e
Stephen Scheiman <147669992+stephen-scheiman@users.noreply.github.com>
2024-03-12 07:53:57 -0700

    Initial build of connection and user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought.js");
+const validator = require("validator");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: "Email address is required",
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: [validator.isEmail, "Please provide a valid email address"],
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return userSchema.friends.length;
+});
