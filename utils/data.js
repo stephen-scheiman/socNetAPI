7cfdb7a58d4ccbc263e2ee5a19cbd9dce8402155
Stephen Scheiman <147669992+stephen-scheiman@users.noreply.github.com>
2024-03-12 13:37:53 -0700

    Got seed data and script from prior exercise

---
+++ utils/data.js
@@ -0,0 +1,83 @@
+const names = [
+  'Aaran',
+  'Aaren',
+  'Aarez',
+  'Aarman',
+  'Aaron',
+  'Aaron-James',
+  'Aarron',
+  'Aaryan',
+  'Aaryn',
+  'Aayan',
+  'Aazaan',
+  'Abaan',
+  'Abbas',
+  'Abdallah',
+  'Abdalroof',
+  'Abdihakim',
+  'Abdirahman',
+  'Abdisalam',
+  'Abdul',
+  'Abdul-Aziz',
+  'Abdulbasir',
+  'Abdulkadir',
+  'Abdulkarem',
+  'Smith',
+  'Jones',
+  'Coollastname',
+  'enter_name_here',
+  'Ze',
+  'Zechariah',
+  'Zeek',
+  'Zeeshan',
+  'Zeid',
+  'Zein',
+  'Zen',
+  'Zendel',
+  'Zenith',
+  'Zennon',
+  'Zeph',
+  'Zerah',
+  'Zhen',
+  'Zhi',
+  'Zhong',
+  'Zhuo',
+  'Zi',
+  'Zidane',
+  'Zijie',
+  'Zinedine',
+  'Zion',
+  'Zishan',
+  'Ziya',
+  'Ziyaan',
+  'Zohaib',
+  'Zohair',
+  'Zoubaeir',
+  'Zubair',
+  'Zubayr',
+  'Zuriel',
+  'Xander',
+  'Jared',
+  'Grace',
+  'Alex',
+  'Mark',
+  'Tamar',
+  'Farish',
+  'Sarah',
+  'Nathaniel',
+  'Parker',
+];
+
+
+const users = [];
+
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Gets a random full name
+const getRandomName = () =>
+  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+
+
+// Export the functions for use in seed.js
+module.exports = { getRandomName };
---
+++ utils/seed.js
@@ -0,0 +1,33 @@
+const connection = require('../config/connection');
+const { User } = require('../models');
+const { getRandomName } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  // Delete the collections if they exist
+  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+  if (userCheck.length) {
+    await connection.dropCollection('users');
+  }
+
+  const users = [];
+
+  for (let i = 0; i < 20; i++) {
+    const fullName = getRandomName();
+    const first = fullName.split(' ')[0];
+    const last = fullName.split(' ')[1];
+
+    users.push({
+      first,
+      last,
+    });
+  }
+
+  await User.collection.insertMany(users);
+
+  console.table(users);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
